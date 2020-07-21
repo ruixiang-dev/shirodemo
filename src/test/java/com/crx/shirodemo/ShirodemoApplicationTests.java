164d15efc5e6535aa4d6994415c9ff1370a8d5f0
Your Name <you@example.com>
2020-07-21 18:05:37 +0800

    first commit

---
+++ src/test/java/com/crx/shirodemo/ShirodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crx.shirodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShirodemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
